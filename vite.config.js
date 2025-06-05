0d4bfd53c58d849dcc84d0618c0cc7e94f77ecc2
break1145 <breakgoogle@163.com>
2025-06-05 21:29:59 +0800

    [fix] update scripts

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  preview: {
+    host: true,
+    port: 5173,  // 你想要的端口
+  },
   plugins: [react()],
 })
